d128e1b0a9a25b3208d23b338becf868a2b82eb5
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2021-01-20 18:00:30 -0300

    Reactive: Setting and Patching Values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,12 +26,27 @@ export class AppComponent implements OnInit {
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
     });
-    this.signupForm.valueChanges.subscribe(res => console.log(res));
-    this.signupForm.statusChanges.subscribe(res => console.log(res));
+    //this.signupForm.valueChanges.subscribe(res => console.log(res));
+    //this.signupForm.statusChanges.subscribe(res => console.log(res));
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Max',
+        'email': 'max@test.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    });
+
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Anna',
+      }
+    });
   }
 
   onSubmit() {
     console.log(this.signupForm)
+    this.signupForm.reset();
   }
 
   onAddHobby() {
